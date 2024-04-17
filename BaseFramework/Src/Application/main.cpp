5b8351b0055b47d434be615337e39a53b742eeea
mokkun2005 <kd1337788@st.kobedenshi.ac.jp>
2024-04-17 10:30:49 +0900

    フレームワーク改
    いらんやつ

--- BaseFramework/Src/Application/main.cpp
+++ BaseFramework/Src/Application/main.cpp
@@ -221,6 +221,9 @@ bool Application::Init(int w, int h)
 	//===================================================================
 	KdAudioManager::Instance().Init();
 
+	//あとで消す
+	m = 0;
+
 	return true;
 }
 
